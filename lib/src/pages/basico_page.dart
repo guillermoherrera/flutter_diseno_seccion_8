7218de84ff952e76a88c5661f9f35a8c84b7046b
Guillermo <guillhermoherrera@gmail.com>
2020-08-20 13:03:55 -0500

    basico 3
    safearea, singlechildscrollview, landscape

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -9,45 +9,53 @@ class BasicoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
           _crearImagen(),
           _crearTitulo(),
           _crearAcciones(),
           _crearTextO(),
           _crearTextO(),
           _crearTextO(),
-          _crearTextO(),
-          
+          _crearTextO(),          
         ],
+        )
       ),      
     );
   }
 
   Widget _crearImagen(){
-    return Image(
-      image: NetworkImage('https://drscdn.500px.org/photo/307987393/m%3D900/v2?sig=eb7565903640d1c12f1c57ef9562a3c2f4522eecdadf3441d20a566fe0ebb94a')
+    return Container(
+      width: double.infinity,
+      child: Image(
+        image: NetworkImage('https://drscdn.500px.org/photo/307987393/m%3D900/v2?sig=eb7565903640d1c12f1c57ef9562a3c2f4522eecdadf3441d20a566fe0ebb94a'),
+        height: 200.0,
+        fit: BoxFit.cover,
+      ),
     );
   }
 
   Widget _crearTitulo(){
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text('Imagen Bonita', style: estiloT),
-                SizedBox(height: 7.0,),
-                Text('Descripción de la imagen bonita', style: estiloST),
-              ],
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Imagen Bonita', style: estiloT),
+                  SizedBox(height: 7.0,),
+                  Text('Descripción de la imagen bonita', style: estiloST),
+                ],
+              ),
             ),
-          ),
-          Icon(Icons.star, color: Colors.red, size: 30.0,),
-          Text('41', style: TextStyle(fontSize: 20.0))
-        ],
+            Icon(Icons.star, color: Colors.red, size: 30.0,),
+            Text('41', style: TextStyle(fontSize: 20.0))
+          ],
+        ),
       ),
     );
   }
@@ -64,11 +72,13 @@ class BasicoPage extends StatelessWidget {
   }
 
   Widget _crearTextO(){
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 20.0),
-      child: Text(
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      textAlign: TextAlign.justify,
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 40.0, vertical: 0.0),
+        child: Text(
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        textAlign: TextAlign.justify,
+        ),
       ),
     );
   }
