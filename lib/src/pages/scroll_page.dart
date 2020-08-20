95025f1c28d535e147601d1445ace531c1dcf8ed
Guillermo <guillhermoherrera@gmail.com>
2020-08-20 13:51:35 -0500

    scroll 2
    stack, imagen, expanded

--- lib/src/pages/scroll_page.dart
+++ lib/src/pages/scroll_page.dart
@@ -16,8 +16,46 @@ class ScrollPage extends StatelessWidget {
   }
 
   Widget _pagina1(){
-    return Center(
-      child: Text('Pagina 1'),
+    return Stack(
+      children: <Widget>[
+        _colorFondo(),
+        _imagenFondo(),
+        _textos(),
+      ],
+    );
+  }
+
+  Widget _colorFondo(){
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: Color.fromRGBO(108, 192, 218, 1),
+    );
+  }
+
+  Widget _imagenFondo(){
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      child: Image(
+        image: AssetImage('assets/scroll-1.png'),
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget _textos(){
+    final styleText = TextStyle(color: Colors.white, fontSize: 50.0);
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 20.0,),
+          Text('11°', style:  styleText),
+          Text('Miércoles', style:  styleText),
+          Expanded(child: Container()),
+          Icon(Icons.keyboard_arrow_down, size: 70.0, color: Colors.white,)
+        ],
+      ),
     );
   }
 
