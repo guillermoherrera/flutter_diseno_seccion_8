53c9531aa572753ba446c77976e171d1e16f5773
Guillermo <guillhermoherrera@gmail.com>
2020-08-20 13:28:27 -0500

    scroll 1
    PageView

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_diseno_seccion_8/src/pages/basico_page.dart';
+import 'package:flutter_diseno_seccion_8/src/pages/scroll_page.dart';
  
 void main() => runApp(MyApp());
  
@@ -10,9 +11,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      initialRoute: 'basico',
+      initialRoute: 'scroll',
       routes: {
-        'basico': (BuildContext context) => BasicoPage()
+        'basico': (BuildContext context) => BasicoPage(),
+        'scroll': (BuildContext context) => ScrollPage()
       },
     );
   }
